eff195e8926980be0d9527c7bce723e1d0481c67
christopher-henderson <chris@chenderson.org>
2023-06-18 07:15:41 -0700

    makes a bit more sense

--- bread.py
+++ bread.py
@@ -1,84 +1,111 @@
-class Gram(int):
-	pass
+class Gram(float):
+    pass
 
 
-class Calorie(int):
-	pass
+class Calorie(float):
+    pass
 
 
-class Nutrition:
+class NutritionalLabel:
 
-	SERVING_SIZE: Gram = 1
+    def __init__(self, serving_size: Gram, calories: Calorie, carbs: Gram, protein: Gram, fat: Gram, fiber: Gram):
+        self.serving_size = serving_size
+        self.calories = calories
+        self.carbs = carbs
+        self.protein = protein
+        self.fat = fat
+        self.fiber = fiber
 
-	CALORIES:Calorie   = 0
-	CARBS: Gram        = 0
-	PROTEIN: Gram      = 0
-	FAT: Gram          = 0
-	FIBER: Gram        = 0
-
-	def __init__(self, grams: Gram):
-		percentage = grams / self.SERVING_SIZE
-		self.calories = percentage * self.CALORIES
-		self.carbs = percentage * self.CARBS
-		self.protein = percentage * self.PROTEIN
-		self.fat = percentage * self.FAT
-		self.fiber = percentage * self.FIBER
-
-	def __add__(self, other):
-		total = Nutrition(0)
-		total.calories = self.calories + other.calories
-		total.carbs = self.carbs + other.carbs
-		total.protein = self.protein + other.protein
-		total.fat = self.fat + other.fat
-		total.fiber = self.fiber + other.fiber
-		return total
-
-	def __str__(self):
-		return f'''\
-calories: {self.calories:.2f}
-carbs:    {self.carbs:.2f}
-protein:  {self.protein:.2f}
-fat:      {self.fat:.2f}
-fiber:    {self.fiber:.2f}'''
+    def macros(self, grams: Gram):
+        percentage = grams / self.serving_size
+        return Macros(calories=percentage * self.calories,
+                      carbs=percentage * self.carbs,
+                      protein=percentage * self.protein,
+                      fat=percentage * self.fat,
+                      fiber=percentage * self.fiber)
 
 
-class WholeWheat(Nutrition):
+class Macros:
 
-	SERVING_SIZE: Calorie = 38
+    def __init__(self, calories: Calorie, carbs: Gram, protein: Gram, fat: Gram, fiber: Gram):
+        self.calories = calories
+        self.carbs = carbs
+        self.protein = protein
+        self.fat = fat
+        self.fiber = fiber
 
-	CALORIES: Gram        = 140
-	CARBS: Gram           = 27
-	PROTEIN: Gram         = 6
-	FAT: Gram             = 0.5
-	FIBER: Gram           = 5
+    def __add__(self, other):
+        return Macros(calories=self.calories + other.calories,
+                      carbs=self.carbs + other.carbs,
+                      protein=self.protein + other.protein,
+                      fat=self.fat + other.fat,
+                      fiber=self.fiber + other.fiber)
+
+    def __str__(self):
+        return f'''\
+    calories: {self.calories:.2f}
+    carbs:    {self.carbs:.2f}
+    protein:  {self.protein:.2f}
+    fat:      {self.fat:.2f}
+    fiber:    {self.fiber:.2f}'''
 
 
-class WhiteWheat(Nutrition):
+WholeWheatNutrition = NutritionalLabel(serving_size=Gram(38),
+                                       calories=Calorie(140),
+                                       carbs=Gram(27),
+                                       protein=Gram(6),
+                                       fat=Gram(0.5),
+                                       fiber=Gram(5))
 
-	SERVING_SIZE: Calorie = 36
+WhiteWheatNutrition = NutritionalLabel(serving_size=Gram(36),
+                                       calories=Calorie(130),
+                                       carbs=Gram(26),
+                                       protein=Gram(5),
+                                       fat=Gram(0.5),
+                                       fiber=Gram(1))
 
-	CALORIES: Gram        = 130
-	CARBS: Gram           = 26
-	PROTEIN: Gram         = 5
-	FAT:Gram              = 0.5
-	FIBER: Gram           = 1
+StarterNutrition = WholeWheatNutrition
 
-class Starter(WholeWheat):
-	pass
+
+class Ingredient:
+    def __init__(self, label: NutritionalLabel, grams: Gram):
+        self.grams = grams
+        self.label = label
+
+    def macros(self):
+        return self.label.macros(self.grams)
+
+
+class WholeWheat(Ingredient):
+
+    def __init__(self, grams: Gram):
+        Ingredient.__init__(self, WholeWheatNutrition, grams)
+
+
+class WhiteWheat(Ingredient):
+
+    def __init__(self, grams: Gram):
+        Ingredient.__init__(self, WhiteWheatNutrition, grams)
+
+
+class Starter(Ingredient):
+
+    def __init__(self, grams: Gram):
+        Ingredient.__init__(self, StarterNutrition, grams)
 
 
 class Sourdough:
 
-	def __init__(self, starter: Starter, whole: WholeWheat, white: WhiteWheat):
-		self.starter = starter
-		self.whole = whole
-		self.white = white
+    def __init__(self, starter: Starter, whole: WholeWheat, white: WhiteWheat):
+        self.starter = starter
+        self.whole = whole
+        self.white = white
 
-	def nutrition(self) -> Nutrition:
-		return self.starter + self.whole + self.white
+    def macros(self) -> Macros:
+        return self.starter.macros() + self.whole.macros() + self.white.macros()
 
-	def __repr__(self):
-		return str(self)
+    def __repr__(self):
+        return str(self)
 
-	def __str__(self):
-		return str(self.nutrition())
+    def __str__(self):
+        return str(self.macros())
